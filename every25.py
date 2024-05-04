7c7100b05fdfc119cae3b6e2a8f49a777b3aac65
alex sanchez <alexjsanchez1020@gmail.com>
2024-05-03 23:31:32 -0400

    input output folder fix

--- every25.py
+++ every25.py
@@ -1,29 +1,30 @@
 import os
-#subprocess is built into python 3.5
 import subprocess
-
-# this function takes in images from input_folder and outputs them into output_folder in the format of bpm
-# destination of folders is up to you. if you do not decide one will be created in the os folder
-
 
 def convert_bmp_to_pbm(input_folder, output_folder):
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
+    # Initialize a counter to keep track of the number of files processed
+    counter = 0
+
     # Iterate over files in the input folder
     for filename in os.listdir(input_folder):
-        #if your images are in different format simply replace the end switch
         if filename.endswith(".bmp"):
+            counter += 1
+            # Skip files until the 25th file is reached
+            if counter % 20 != 0:
+                continue
+            
             input_path = os.path.join(input_folder, filename)
             output_filename = os.path.splitext(filename)[0] + ".pbm"
             output_path = os.path.join(output_folder, output_filename)
-            # Convert the image using ImageMagick
             subprocess.run(["magick", "convert", input_path, output_path])
 
 # Specify the input and output folders
-input_folder = "/users/alexa/documents/"
-output_folder = "/users/alexa/documents/"
+input_folder = "/users/alexa/documents/BlvckHvrts"
+output_folder = "/users/alexa/documents/video_25"
 
-# Convert BMP images in the input folder
+# Convert every 25th BMP image in the input folder
 convert_bmp_to_pbm(input_folder, output_folder)
